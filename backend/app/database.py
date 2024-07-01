6ab90eea06d4b6583f8eb4f6e87987a61b35759f
Akshat-Vashisht <akashat2003@gmail.com>
2024-07-01 16:28:02 +0530

    fix: added .env to .gitignore

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
+import os
+import dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/RediVS"
+dotenv.load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URI')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-

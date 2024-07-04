dc0544ecad5b20b1873c62cf158d7b83bd5637e0
Akshat-Vashisht <akashat2003@gmail.com>
2024-07-04 09:56:35 +0530

    fix: CORS error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from .api.container_master.router import router as container_master_router
 
 app = FastAPI(debug=True)
 app.include_router(container_master_router)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.get('/health-check')
 async def root():

9ffda4bd95f89c3c3fabb87825befa5ba6e1cb80
Akshat-Vashisht <akashat2003@gmail.com>
2024-07-10 15:26:40 +0530

    CORS fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,10 @@ app.include_router(pick_list_details_router)
 app.include_router(auth_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=[
+        'http://127.0.0.1:3000',
+        'http://localhost:3000'
+    ],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],

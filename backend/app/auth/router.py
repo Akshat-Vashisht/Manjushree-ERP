a1780af5566a2023453ea677f6b1862dae2371d5
Akshat-Vashisht <akashat2003@gmail.com>
2024-07-16 18:13:11 +0530

    fix(CORS): use starlette CORSmiddleware

--- backend/app/auth/router.py
+++ backend/app/auth/router.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from .schemas import UserCreate, Token, UserLogin, UserResponse
@@ -35,8 +35,10 @@ async def register_user(user: UserCreate, db: Session = Depends(get_db)):
 async def login_for_access_token(user_login: UserLogin, response: Response, db: Session = Depends(get_db)):
     user = authenticate_user(db, user_login.user_name, user_login.password)
     if not user:
-        JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={
-                     "detail": "Incorrect username or password"})
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password"
+        )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.user_name}, expires_delta=access_token_expires
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from .api.container_master.router import router as container_master_router
 from .api.container_movement_master.router import router as container_movement_router
 from .api.scan_location_master.router import router as scan_location_master_router

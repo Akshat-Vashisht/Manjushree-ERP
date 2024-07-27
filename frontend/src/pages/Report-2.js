4f80ae1c729fac72ae6c290c5ec0995752b046a3
Akshat-Vashisht <akashat2003@gmail.com>
2024-07-27 16:03:23 +0530

    fix(report): set column names for proper display of date and time

--- frontend/src/pages/Report-2.js
+++ frontend/src/pages/Report-2.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { axiosConfig } from "../axios/axiosConfig";
 import { Table } from "antd";
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 
 const Report2 = () => {
   const [dataSource, setDataSource] = useState([]);
@@ -11,25 +11,72 @@ const Report2 = () => {
     try {
       const res = await axiosConfig.get("/reports/containers-at-manjushree");
       setDataSource(res.data);
-      setColumns(
-        Object.keys(res.data[0] || {}).map((item) => ({
-          title: item.split("_").join(" ").toUpperCase(),
-          dataIndex: item,
-          key: item,
-        }))
-      );
+      setColumns([
+        {
+          title: "Date",
+          dataIndex: "datetime",
+          key: "date",
+          render: (text) =>
+            new Date(text)
+              .toLocaleDateString()
+              .replace("/", "-")
+              .replace("/", "-"),
+        },
+        {
+          title: "Time",
+          dataIndex: "datetime",
+          key: "time",
+          render: (text) => new Date(text).toLocaleTimeString(),
+        },
+        {
+          title: "Container Location",
+          dataIndex: "container_location",
+          key: "container_location",
+        },
+        {
+          title: "Container Category",
+          dataIndex: "container_category",
+          key: "container_category",
+        },
+        {
+          title: "Container Code",
+          dataIndex: "container_code",
+          key: "container_code",
+        },
+        {
+          title: "RFID Tag No",
+          dataIndex: "rfid_tag_no",
+          key: "rfid_tag_no",
+        },
+        {
+          title: "SKU Code",
+          dataIndex: "sku_code",
+          key: "sku_code",
+          render(text) {
+            return text ? text : "-";
+          },
+        },
+        {
+          title: "Pick List Code",
+          dataIndex: "pick_list_code",
+          key: "pick_list_code",
+          render(text) {
+            return text ? text : "-";
+          },
+        },
+      ]);
     } catch (error) {
       console.error("ERR::GET::MANJUSHREE");
     }
   }
   useEffect(() => {
     getAllContainersAtManjushree();
-  }, [])
-  
+  }, []);
+
   return (
     <Layout>
       <h1 className="font-semibold text-lg mb-5">Containers at Manjushree</h1>
-      <hr className="mb-2 text-slate-700" />
+      <hr className="mb-5 text-slate-700" />
 
       <Table
         dataSource={dataSource}

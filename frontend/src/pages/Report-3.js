38055e4eca102f0b1563b8200ff12b958cf0c194
Akshat-Vashisht <akashat2003@gmail.com>
2024-07-27 16:03:35 +0530

    fix(report): fix issues with report 3 1. Add column names to display date and time appropriately 2. Removed deprecated code

--- frontend/src/pages/Report-3.js
+++ frontend/src/pages/Report-3.js
@@ -27,30 +27,56 @@ const Report3 = () => {
       const res = await axiosConfig.get(
         `/reports/get-client-report/${selectedOption}`
       );
-      setDataSource(res.data);
-      setColumns(
-        Object.keys(res.data[0] || {}).map((item) => ({
-          title: item.split("_").join(" ").toUpperCase(),
-          dataIndex: item,
-          key: item,
-        }))
-      );
+      setDataSource(res.data.map((item, index) => ({ ...item, key: index })));
+      setColumns([
+        {
+          title: "Business Entity Name",
+          dataIndex: "business_entity_name",
+          key: "business_entity_name",
+        },
+        {
+          title: "Date",
+          dataIndex: "datetime",
+          key: "date",
+          render: (text) =>
+            new Date(text)
+              .toLocaleDateString()
+              .replace("/", "-")
+              .replace("/", "-"),
+        },
+        {
+          title: "Time",
+          dataIndex: "datetime",
+          key: "time",
+          render: (text) => new Date(text).toLocaleTimeString(),
+        },
+        {
+          title: "Container Category",
+          dataIndex: "container_category",
+          key: "container_category",
+        },
+        {
+          title: "Container Code",
+          dataIndex: "container_code",
+          key: "container_code",
+        },
+        {
+          title: "RFID Tag No",
+          dataIndex: "rfid_tag_no",
+          key: "rfid_tag_no",
+        },
+      ]);
     } catch (error) {
       console.error("ERR::GET::ALL");
     }
   }
 
-  const menu = (
-    <Menu
-      items={option.map((e) => ({
-        key: e.key,
-        label: e.label,
-        onClick: () => {
-          setSelectedOption(e.label);
-        },
-      }))}
-    />
-  );
+  const items = option.map((item) => ({
+    key: item.key,
+    label: item.label,
+    onClick: () => setSelectedOption(item.label),
+  }));
+
   useEffect(() => {
     getAllClientOptions();
   }, []);
@@ -63,10 +89,10 @@ const Report3 = () => {
       <h1 className="font-semibold text-lg mb-5">
         Containers Detail Client Wise
       </h1>
-      <hr className="mb-2 text-slate-700" />
+      <hr className="mb-5 text-slate-700" />
       <Dropdown
-        className="border border-slate-400 rounded-md w-[10rem] px-3 py-1 my-2"
-        overlay={menu}
+        className="border border-slate-400 rounded-md w-[10rem] px-3 py-1 my-5"
+        menu={{ items }}
         trigger={["click"]}
       >
         <a

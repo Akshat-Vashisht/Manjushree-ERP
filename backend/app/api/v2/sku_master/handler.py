c3e049404e719955df1ffde0a8bbc52a56293b2c
Akshat-Vashisht <akashat2003@gmail.com>
2024-08-14 16:28:14 +0530

    fix(sku): update schema and handler to adhere to new constraints

--- backend/app/api/v2/sku_master/handler.py
+++ backend/app/api/v2/sku_master/handler.py
@@ -1,37 +1,71 @@
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from ....models import SKUMaster
 from ....utils import now, fetch_data
-from .schemas import SKUCreateSchema
+from .schemas import SKUCreateSchema, SKUSchema
 import datetime
 
 
-def fetch_scan_locations(db: Session):
-    sku_list = db.query(SKUMaster).all()
-    return sku_list
+def fetch_scan_locations(db: Session, page: int, page_size: int):
+    offset = page_size * (page - 1)
+
+    _query = db.query(SKUMaster)
+
+    record_count = _query.count()
+
+    pagination = {
+        "total_pages": record_count / page_size,
+        "current_page": page,
+        "total_records": record_count,
+        "records": [SKUSchema.model_validate(each) for each in _query.offset(offset).limit(page_size)]
+    }
+
+    return pagination
 
 
 def add_sku(db: Session, sku_input: list[SKUCreateSchema], last_updated_by: int):
 
     new_skus = []
     for sku in sku_input:
-        data = SKUMaster(
-            sku_code=sku.sku_code,
-            sku_name=sku.sku_name,
-            is_active=True,
-            last_updated_dt=now(return_string=True),
-            last_updated_by=last_updated_by
+        exists = (
+            db.query(func.count())
+            .select_from(SKUMaster)
+            .where(SKUMaster.sku_code == sku.sku_code)
+            .scalar()
         )
 
-        new_skus.append(data)
+        if exists > 0:
+            continue  # Skip this entry if it already exists
 
-    db.bulk_save_objects(new_skus)
-    db.commit()
+        data = sku.model_dump()
+        data.update({
+            'is_active': True,
+            'last_updated_dt': now(return_string=True),
+            'last_updated_by': last_updated_by
+        })
+
+        new_sku = SKUMaster(**data)
+        new_skus.append(new_sku)
+
+    if new_skus:
+        db.bulk_save_objects(new_skus)
+        db.commit()
 
     return new_skus
 
 
 def update_sku(id: int, sku_input: SKUCreateSchema, last_updated_by: int, db: Session):
     sku = fetch_data(db, SKUMaster, 'sku_master_id', id)
+
+    exists = (
+        db.query(func.count())
+        .select_from(SKUMaster)
+        .where(SKUMaster.sku_code == sku_input.sku_code)
+        .scalar()
+    )
+
+    if exists > 0:
+        raise ValueError("SKU with given code already exists")
 
     sku.sku_code = sku_input.sku_code
     sku.sku_name = sku_input.sku_name
--- backend/app/api/v2/sku_master/router.py
+++ backend/app/api/v2/sku_master/router.py
@@ -7,6 +7,7 @@ from .schemas import SKUSchema, SKUCreateSchema
 from ....utils import get_db
 from .handler import fetch_scan_locations, add_sku, update_sku, soft_delete_sku
 from ....auth.handler import get_current_user
+from ....exceptions import DataNotFoundError
 
 router = APIRouter()
 router.dependencies = [Depends(get_current_user)]
@@ -15,16 +16,19 @@ router.dependencies = [Depends(get_current_user)]
 @ router.get('/', response_model=list[SKUSchema])
 async def get_skus(
     db: Session = Depends(get_db),
+    page: int = 1,
+    page_size: int = 10,
     user=Depends(get_current_user)
 ):
     try:
-        scan_locations = jsonable_encoder(fetch_scan_locations(db))
-        return JSONResponse(status_code=status.HTTP_200_OK, content={'detail': scan_locations})
+        scan_locations = jsonable_encoder(
+            fetch_scan_locations(db, page, page_size))
+        return JSONResponse(status_code=status.HTTP_200_OK, content=scan_locations)
     except Exception as e:
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={'detail': str(e)})
 
 
-@ router.post('/', status_code=status.HTTP_201_CREATED, response_model=SKUSchema)
+@ router.post('/', status_code=status.HTTP_201_CREATED, response_model=list[SKUSchema])
 async def create_sku(
     sku_input: list[SKUCreateSchema],
     db: Session = Depends(get_db),
@@ -32,7 +36,14 @@ async def create_sku(
 ):
 
     # TODO: Change hardcoded created_by value to retrieve value from current user
-    return add_sku(db, sku_input, user.user_master_id)
+    try:
+        result = jsonable_encoder(add_sku(
+            db, sku_input, user.user_master_id))
+        if not result:
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'No unique sku code found.'})
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=result)
+    except Exception as e:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={'detail': str(e)})
 
 
 @ router.patch('/{id}', response_model=SKUSchema)
@@ -44,7 +55,16 @@ async def update_sku_details(
 ):
 
     # TODO: Change hardcoded created_by value to retrieve value from current user
-    return update_sku(id, sku_input, user.user_master_id, db)
+    try:
+        result = jsonable_encoder(update_sku(
+            id, sku_input, user.user_master_id, db))
+        return JSONResponse(status_code=status.HTTP_200_OK, content=result)
+    except DataNotFoundError as e:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'detail': str(e)})
+    except ValueError as e:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': str(e)})
+    except Exception as e:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={'detail': str(e)})
 
 
 @ router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
@@ -55,4 +75,9 @@ async def delete_sku(
 ):
 
     # TODO: Change hardcoded created_by value to retrieve value from current user
-    soft_delete_sku(id, user.user_master_id, db)
+    try:
+        soft_delete_sku(id, user.user_master_id, db)
+    except DataNotFoundError as e:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'detail': str(e)})
+    except Exception as e:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={'detail': str(e)})
